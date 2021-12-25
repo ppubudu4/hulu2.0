a4ce4314217199d440d974e1c3882f62dcce3964
Pubudu Dananjaya <ppubudu4@gmail.com>
2021-12-26 04:46:15 +0530

    return

--- components/Results.js
+++ components/Results.js
@@ -5,9 +5,9 @@ import FlipMove from 'react-flip-move';
 const Results = ({ results }) => {
   return (
     <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
-      {results?.map((item) => {
-        return <Tumbnail key={item.id} result={item} />;
-      })}
+      {results?.map((item) => (
+        <Tumbnail key={item.id} result={item} />
+      ))}
     </FlipMove>
   );
 };
